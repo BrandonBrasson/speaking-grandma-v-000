eed9170a705be361f508b79e78f7ec8e6e0064cc
Brandon Brasson <rocksong622@gmail.com>
2019-01-07 20:39:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,11 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(n)
-  if n===n.upcase
+  if n==n.upcase
   return  "NO, NOT SINCE 1938!"
 elsif n!=n.upcase
   return"HUH?! SPEAK UP, SONNY!"
-else n==n.downcase
+else
   return"I LOVE YOU TOO PUMPKIN!"
   end
 end
