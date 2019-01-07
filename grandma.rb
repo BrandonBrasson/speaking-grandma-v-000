5ef0ce864b762c46f0b51556fed905f86209de4d
Brandon Brasson <rocksong622@gmail.com>
2019-01-07 20:10:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma(n)
   if n == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif  n == n.downcase
+  elsif  n == n.upcase
     "NO, NOT SINCE 1938!"
-  elsif  n == n.downcase
+  elsif  n == n.upcase
     "HUH?! SPEAK UP, SONNY!"
   end
 end
